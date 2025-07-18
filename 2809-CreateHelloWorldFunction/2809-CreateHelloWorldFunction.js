6fd34d48abf9fcd3a4a381785180b03c12f62b5e
Raghul Kannan <120631105+raghulkannan-s@users.noreply.github.com>
2025-07-18 19:00:27 +0530

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,17 @@
+// Last updated: 7/18/2025, 7:00:24 PM
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+   
+
+    return  function koopudu(){
+        return "Hello World"
+    }
+};
+
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
