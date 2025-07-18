cf070214c2d47d8bbec7505a1ba457140ff10745
Raghul Kannan <120631105+raghulkannan-s@users.noreply.github.com>
2025-07-18 19:00:41 +0530

    Create 742-ToLowerCase

---
+++ 742-ToLowerCase/742-ToLowerCase.py
@@ -0,0 +1,9 @@
+# Last updated: 7/18/2025, 7:00:37 PM
+class Solution(object):
+    def toLowerCase(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return s.lower()
+        
