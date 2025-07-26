bbb5e2cebf2abc04dbffd24764533b877634476d
Raghul Kannan <120631105+raghulkannan-s@users.noreply.github.com>
2025-07-26 23:36:52 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.py
@@ -0,0 +1,9 @@
+# Last updated: 7/26/2025, 11:36:47 PM
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return sorted(s) == sorted(t)
