40a42525887a0128ba5c33362ac4aa9278953d87
Raghul Kannan <120631105+raghulkannan-s@users.noreply.github.com>
2025-11-17 00:55:01 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/version/java/9-PalindromeNumber_20251117_005459.java
@@ -0,0 +1,18 @@
+// Last updated: 11/17/2025, 12:54:59 AM
+class Solution {
+
+
+    public boolean isPalindrome(int x) {
+        if(x==0) return true;
+        if(x < 0 || x%10 == 0) return false;
+        
+        int rev = 0;
+
+        while( rev < x ){
+            rev = rev * 10 + (x%10);
+            x/=10;
+        }
+        return ( x == rev || x == rev / 10 );
+    
+    }
+}
