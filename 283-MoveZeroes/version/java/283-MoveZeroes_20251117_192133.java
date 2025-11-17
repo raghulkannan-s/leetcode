7af5e1ea29c5466d8ed7c2c0d2667b9fc64bbdc0
Raghul Kannan <120631105+raghulkannan-s@users.noreply.github.com>
2025-11-17 19:21:38 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/version/java/283-MoveZeroes_20251117_192133.java
@@ -0,0 +1,25 @@
+// Last updated: 11/17/2025, 7:21:33 PM
+class Solution {
+
+    public void moveZeroes(int[] nums) {
+        
+        int write = 0;
+
+
+        for ( int read = 0; read<nums.length; read++ ){
+
+           if( nums[read] != 0 ){
+             nums[write] = nums[read];
+             write++;
+           }
+
+        }
+
+           while( write<nums.length ){
+             nums[write] = 0;
+             write++;
+           }
+
+
+    }
+}
