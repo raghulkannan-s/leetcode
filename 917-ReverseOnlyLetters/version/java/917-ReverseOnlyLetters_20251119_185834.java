bad1edf10db40dc9eb354a3ad4c99a0c136b5b86
Raghul Kannan <120631105+raghulkannan-s@users.noreply.github.com>
2025-11-19 18:58:42 +0530

    Create 917-ReverseOnlyLetters

---
+++ 917-ReverseOnlyLetters/version/java/917-ReverseOnlyLetters_20251119_185834.java
@@ -0,0 +1,24 @@
+// Last updated: 11/19/2025, 6:58:34 PM
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n; 
+
+        reverse( nums, 0, n-1 );
+        reverse( nums, 0, k-1 );
+        reverse( nums, k, n-1 );
+
+    }
+
+
+    private void reverse(int[] nums, int left, int right){
+        while( left < right ){
+
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+
+            left++; right--;
+        }
+    }
+}
