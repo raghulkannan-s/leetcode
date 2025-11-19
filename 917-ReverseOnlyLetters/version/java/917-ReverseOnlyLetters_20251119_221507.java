dfd75747469cbe9a81a538e18301705f8a21a3d7
Raghul Kannan <120631105+raghulkannan-s@users.noreply.github.com>
2025-11-19 22:15:15 +0530

    Create 917-ReverseOnlyLetters

---
+++ 917-ReverseOnlyLetters/version/java/917-ReverseOnlyLetters_20251119_221507.java
@@ -0,0 +1,23 @@
+// Last updated: 11/19/2025, 10:15:07 PM
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int ans[] = new int[n];
+
+        ans[0] = 1;
+
+        for( int i = 1; i<n; i++ ){ 
+            ans[i] = ans[i-1] * nums[i-1];
+        }
+
+        int end = 1;
+
+        for (int i = n-1; i>=0; i--){
+            ans[i] = ans[i]*end;
+            end = end*nums[i];
+        } 
+        
+        return ans;
+
+    }
+}
