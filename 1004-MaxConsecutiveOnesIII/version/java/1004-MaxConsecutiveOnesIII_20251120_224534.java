9340f8165af846af836ce1ecbdd6ad7a7493b101
Raghul Kannan <120631105+raghulkannan-s@users.noreply.github.com>
2025-11-20 22:45:44 +0530

    Create 1004-MaxConsecutiveOnesIII

---
+++ 1004-MaxConsecutiveOnesIII/version/java/1004-MaxConsecutiveOnesIII_20251120_224534.java
@@ -0,0 +1,31 @@
+// Last updated: 11/20/2025, 10:45:34 PM
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+
+        for( int i = 0; i<nums.length-2; i++ ){
+            if( i > 0 && nums[i] == nums[i-1] ) continue;
+            int left = i+1;
+            int right = nums.length-1;
+
+            while( left < right ){
+                int sum = nums[i] + nums[right] + nums[left];
+                if( sum == 0 ) {
+                    result.add(Arrays.asList( nums[i], nums[right], nums[left] ));
+
+                    left++;
+                    right--;
+
+                    while( left < right && nums[right] == nums[right+1] ) right--;
+                    while( left < right && nums[left] == nums[left-1] ) left++;
+                }
+                else if( sum < 0 ) left++;
+                else right--;
+                
+            }
+        }
+        return result;
+
+    }
+}
