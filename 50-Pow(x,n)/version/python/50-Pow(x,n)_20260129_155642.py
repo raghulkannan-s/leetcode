d429b9fb8988ac4327921810690c5bc9d23adba1
Raghul Kannan <120631105+raghulkannan-s@users.noreply.github.com>
2026-01-29 15:56:43 +0530

    Create 50-Pow(x,n)

---
+++ 50-Pow(x,n)/version/python/50-Pow(x,n)_20260129_155642.py
@@ -0,0 +1,12 @@
+# Last updated: 1/29/2026, 3:56:42 PM
+1class Solution(object):
+2    def myPow(self, x, n):
+3        """
+4        :type x: float
+5        :type n: int
+6        :rtype: float
+7        """
+8        return x**n
+9
+10
+11        
