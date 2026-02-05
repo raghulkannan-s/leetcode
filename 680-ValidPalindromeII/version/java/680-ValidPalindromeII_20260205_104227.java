604c1e4dd289e64dd7720eb68bd995a4640fc53f
Raghul Kannan <120631105+raghulkannan-s@users.noreply.github.com>
2026-02-05 10:42:27 +0530

    Create 680-ValidPalindromeII

---
+++ 680-ValidPalindromeII/version/java/680-ValidPalindromeII_20260205_104227.java
@@ -0,0 +1,53 @@
+// Last updated: 2/5/2026, 10:42:27 AM
+1class Solution {
+2
+3    public boolean checkPalin(String s, int left, int right){
+4
+5        while( left < right ){
+6
+7            if( s.charAt(left) == s.charAt(right)  ){
+8                left++;
+9                right--;
+10            }else{
+11                return false;
+12            }
+13        }
+14        return true;
+15
+16    }
+17
+18    public boolean validPalindrome(String s) {
+19
+20        boolean flag = true;
+21
+22        int left = 0;  
+23        int right =  s.length()-1;
+24
+25
+26        while( left < right ){
+27            
+28        //    if(flag && (s.charAt(left) != s.charAt(right))){
+29
+30        //      if( checkPalin(s, left, right-1) ){
+31        //         right--;
+32        //         flag = false;
+33        //      }else if( checkPalin(s, left+1, right)){
+34        //         left++;
+35        //         flag = false;
+36        //      }
+37
+38        //    }
+39
+40            if( s.charAt(left) == s.charAt(right) ){
+41                left++;
+42                right--;
+43            }
+44            else{
+45                return checkPalin(s, left, right-1) || checkPalin(s, left+1, right) ;
+46            }
+47            
+48        }
+49
+50        return true;
+51    }
+52}
