d892a78835b5d1deb463b4deab7a2adf4bbd3930
Raghul Kannan <120631105+raghulkannan-s@users.noreply.github.com>
2026-02-25 15:17:42 +0530

    Create 3-LongestSubstringWithoutRepeatingCharacters

---
+++ 3-LongestSubstringWithoutRepeatingCharacters/version/java/3-LongestSubstringWithoutRepeatingCharacters_20260225_151741.java
@@ -0,0 +1,29 @@
+// Last updated: 2/25/2026, 3:17:41 PM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public boolean isSameTree(TreeNode p, TreeNode q) {
+18        
+19
+20        if( p == null && q == null ) return true;
+21        if( p == null || q == null ) return false;
+22        if( p.val != q.val) return false;
+23        
+24        return (isSameTree( p.left, q.left ) && isSameTree( p.right, q.right ));
+25
+26
+27    }
+28}
