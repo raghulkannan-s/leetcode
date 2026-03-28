ce0977ea04c82070d2e45fd086ad0b57b12a74cf
Raghul Kannan <120631105+raghulkannan-s@users.noreply.github.com>
2026-03-28 20:54:00 +0530

    Create 516-LongestPalindromicSubsequence

---
+++ 516-LongestPalindromicSubsequence/version/java/516-LongestPalindromicSubsequence_20260328_205326.java
@@ -0,0 +1,27 @@
+// Last updated: 3/28/2026, 8:53:26 PM
+1class Solution {
+2    public int longestPalindromeSubseq(String s) {
+3        
+4        StringBuilder temp = new StringBuilder(s);
+5        String r = temp.reverse().toString();
+6
+7        int n = s.length();
+8
+9        int[] prev = new int[n+1];
+10        for( int i = 1; i<=n; i++ ){
+11            int[] curr = new int[n+1];
+12            for( int j = 1; j<=n; j++ ){
+13                if( s.charAt(i-1) == r.charAt(j-1) ){
+14                    curr[j] = 1 + prev[j-1];
+15                }
+16                else{
+17                    curr[j] = Math.max( prev[j], curr[j-1] );
+18                }
+19            }
+20            prev = curr;
+21        }
+22
+23        return prev[n];
+24
+25    }
+26}
