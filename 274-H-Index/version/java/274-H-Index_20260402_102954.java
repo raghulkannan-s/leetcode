17b491f2ac97f02764768b91c482fb866863739e
Raghul Kannan <120631105+raghulkannan-s@users.noreply.github.com>
2026-04-02 10:29:59 +0530

    Create 274-H-Index

---
+++ 274-H-Index/version/java/274-H-Index_20260402_102954.java
@@ -0,0 +1,19 @@
+// Last updated: 4/2/2026, 10:29:54 AM
+class Solution {
+    public int hIndex(int[] citations) {
+        int papers = citations.length;
+        int[] citationMap = new int[papers + 1];
+
+        for(int citation : citations){
+            citationMap[Math.min(citation,papers)]++;
+        }
+
+        int totalPapers = 0;
+        for(int hIndex = papers; hIndex >= 0; hIndex--){
+            totalPapers += citationMap[hIndex];
+            if(totalPapers >= hIndex)
+                return hIndex;
+        }
+        return 0;
+    }
+}
