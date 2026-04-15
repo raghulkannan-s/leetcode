1359cd408db4d654e614992ccfe7995804382902
Raghul Kannan <120631105+raghulkannan-s@users.noreply.github.com>
2026-04-15 18:44:54 +0530

    Create 700-SearchinaBinarySearchTree

---
+++ 700-SearchinaBinarySearchTree/version/java/700-SearchinaBinarySearchTree_20260415_184450.java
@@ -0,0 +1,28 @@
+// Last updated: 4/15/2026, 6:44:50 PM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public TreeNode searchBST(TreeNode root, int val) {
+18        if( root ==  null ) return root;
+19        if( root.val == val ) return root;
+20        
+21        if( val < root.val ) return searchBST(root.left, val);
+22        if( val > root.val ) return searchBST(root.right, val);
+23
+24        return root;
+25
+26    }
+27}
